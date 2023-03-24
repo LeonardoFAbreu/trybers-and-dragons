2f0d66a3c63d2643080d2490c3bf96a521ca0478
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2023-03-24 16:15:45 -0300

    Adiciona req 02. Cria Classes Dwarf, Elf, Halfling e Orc

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,24 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _instances: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    if (!this._instances) {
+      this._instances = 1;
+      return this._instances;
+    }
+    this._instances += 1;
+    return this._instances;
+  }
+}
---
+++ src/Races/Halfling.ts
@@ -0,0 +1,24 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints: number;
+  private static _instances: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    if (!this._instances) {
+      this._instances = 1;
+      return this._instances;
+    }
+    this._instances += 1;
+    return this._instances;
+  }
+}
